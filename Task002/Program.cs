a71e20732651e84102d6936ce9dadd4622463333
VinogradovMihail <misha-vinogradov-1997@mail.ru>
2022-05-27 14:06:11 +0300

    Выполнена задача N2

---
+++ Task002/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача N2 по определению наибольшего из трех чисел 
+
+Console.Write("Введите три числа, которые хотите сравнить: ");
+
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+int numberC = int.Parse(Console.ReadLine());
+int max = numberA;
+
+if(numberB > max) max = numberB;
+if(numberC > max) max = numberC;
+
+Console.Write("Наибольшим числом является: ");
+Console.Write(max);
