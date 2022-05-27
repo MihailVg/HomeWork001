8d2d02778b9cc7648ff055b35fc7392ada0a2240
VinogradovMihail <misha-vinogradov-1997@mail.ru>
2022-05-27 14:28:12 +0300

    Почти выполнена задача N3

---
+++ Task003/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача N3 по определению четного числа
+
+Console.WriteLine("Введите число, четность которого хотите проверить: ");
+
+int numberA = int.Parse(Console.ReadLine());
+
+if(numberA % 2 = 0)
+{
+    Console.WriteLine("Является четным числом");
+}
+else
+{
+    Console.WriteLine("Является нечетным числом");
+}
