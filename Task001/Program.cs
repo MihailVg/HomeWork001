f8ac2d031b5ac91b0e8570479ecb1b285d9691f9
VinogradovMihail <misha-vinogradov-1997@mail.ru>
2022-05-27 13:48:50 +0300

    Выполнена задача N1

---
+++ Task001/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача N1 по определению большего из двух чисел
+
+Console.Write("Введите два числа, которые хотите сравнить: ");
+
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+
+if(numberA > numberB)
+{
+    Console.Write("Наибольшим числом является: ");
+    Console.Write(numberA);
+}
+else
+{
+    Console.Write("Наибольшим числом является: ");
+    Console.Write(numberB);
+}
