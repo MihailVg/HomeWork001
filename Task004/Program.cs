84d30287fae2d402f69941c452e0ddb0906d36d6
VinogradovMihail <misha-vinogradov-1997@mail.ru>
2022-05-27 14:36:25 +0300

    Выполнена задача N4

---
+++ Task004/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача N4 по определению четных чисел до заданного числа
+
+Console.WriteLine("Введите число, четные до которого вы хотите определить: ");
+
+int numberA = int.Parse(Console.ReadLine());
+int numberB = 1;
+
+while(numberB <= numberA)
+{
+    if(numberB % 2 == 0)
+    {
+        Console.WriteLine(numberB);
+        numberB += 1;
+    }
+    else
+    {
+        numberB += 1;
+    }
+}
